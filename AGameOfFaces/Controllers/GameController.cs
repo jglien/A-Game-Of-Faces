3ce8b6c5d41b8f3906b5d9a2b1dd70b49daaacd4
John Lien <johnglien@gmail.com>
2018-09-20 21:47:48 -0700

    Add controllers based on requirements.

---
+++ AGameOfFaces/Controllers/GameController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AGameOfFaces.Controllers
+{
+    /// <summary>
+    /// Controller for game actions.
+    /// </summary>
+    public class GameController : ApiController
+    {
+        /// <summary>
+        /// Gets the profiles to guess for normal mode.
+        /// </summary>
+        /// <returns>The profiles.</returns>
+        [HttpGet]
+        public IHttpActionResult NormalMode()
+        {
+
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Gets the profiles for Mat(t)-Mode.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IHttpActionResult MattMode()
+        {
+
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Gets the profiles for reverse mode.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IHttpActionResult ReverseMode()
+        {
+
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Gets the profiles for no one mode.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IHttpActionResult NoOneMode()
+        {
+
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Submits the answer.
+        /// </summary>
+        /// <param name="answer"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult Answer([FromBody] string answer)
+        {
+
+
+            return Ok();
+        }
+    }
+}
