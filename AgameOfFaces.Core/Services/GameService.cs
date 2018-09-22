9ad4db720627a094533d13e01bf1736aae44799d
John Lien <johnglien@gmail.com>
2018-09-22 01:34:59 -0700

    Implementing normal game.

--- AgameOfFaces.Core/Services/GameService.cs
+++ AgameOfFaces.Core/Services/GameService.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Text;
 using System.Threading.Tasks;
 using AgameOfFaces.Core.DTO;
 using AgameOfFaces.Core.Enums;
 using AgameOfFaces.Core.Services.Interfaces;
+using AGameOfFaces.Core.DTO;
 
 namespace AgameOfFaces.Core.Services
 {
@@ -36,10 +39,8 @@ namespace AgameOfFaces.Core.Services
             {
                 case Mode.Normal:
                 default:
-                    break;
+                    return GetNormalGameData();
             }
-
-            return new GameData();
         }
 
         #endregion
@@ -50,6 +51,50 @@ namespace AgameOfFaces.Core.Services
         {
             nameof(Mode.Normal)
         });
+
+        #endregion
+
+        #region Private Methods
+
+        private GameData GetNormalGameData()
+        {
+            const int numFaces = 6;
+            var random = new Random();
+            var profiles = GetProfiles().ToList();
+
+            var selectedProfiles = new List<Profile>();
+            for(var i = 0; i < numFaces; i++)
+            {
+                var profile = profiles.ElementAt(random.Next(profiles.Count));
+                selectedProfiles.Add(profile);
+                
+                // Remove so we don't add twice.
+                profiles.Remove(profile);
+            }
+
+            var profileForName = selectedProfiles.ElementAt(random.Next(selectedProfiles.Count));
+            return new GameData
+            {
+                Faces = selectedProfiles.Select(p => p.Headshot.Url),
+                Names = new List<string> { $"{profileForName.FirstName} {profileForName.LastName}" }
+            };
+        }
+
+        private IEnumerable<Profile> GetProfiles()
+        {
+            IEnumerable<Profile> profiles = new List<Profile>();
+
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(@"https://www.willowtreeapps.com/api/v1.0/profiles").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    profiles = response.Content.ReadAsAsync<IEnumerable<Profile>>().Result;
+                }
+            }
+
+            return profiles;
+        }
 
         #endregion
     }
